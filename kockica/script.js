50453ae51f133a80f21bc835f88586133ed6ce51
AhmedFelic <ahmedfelic1@gmail.com>
2022-01-25 01:27:22 +0100

    fix

---
+++ kockica/script.js
@@ -0,0 +1,16 @@
+/*Nije moje - button scroll left right */
+function slide(direction){
+    var container = document.getElementById('container');
+    scrollCompleted = 0;
+    var slideVar = setInterval(function(){
+        if(direction == 'left'){
+            container.scrollLeft -= 350;
+        } else {
+            container.scrollLeft += 350;
+        }
+        scrollCompleted += 350;
+        if(scrollCompleted >= 350){
+            window.clearInterval(slideVar);
+        }
+    }, 50);
+}
