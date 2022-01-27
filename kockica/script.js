c21585e77a3df1b2dfd748a26ac1670a029d5e0c
AhmedFelic <ahmedfelic1@gmail.com>
2022-01-27 18:28:18 +0100

    a

--- kockica/script.js
+++ kockica/script.js
@@ -1,28 +1,22 @@
 /*Nije moje - button scroll left right */
-function slide(direction){
-    var container = document.getElementById('container');
-    scrollCompleted = 0;
-    var slideVar = setInterval(function(){
-        if(direction == 'left'){
-            container.scrollLeft -= 350;
-        } else {
-            container.scrollLeft += 350;
-        }
-        scrollCompleted += 350;
-        if(scrollCompleted >= 350){
-            window.clearInterval(slideVar);
-        }
-    }, 50);
+function slide(direction) {
+  var container = document.getElementById("container");
+  scrollCompleted = 0;
+  var slideVar = setInterval(function () {
+    if (direction == "left") {
+      container.scrollLeft -= 350;
+    } else {
+      container.scrollLeft += 350;
+    }
+    scrollCompleted += 350;
+    if (scrollCompleted >= 350) {
+      window.clearInterval(slideVar);
+    }
+  }, 50);
 }
 /*Skroll back na pocetak na refresh stranice */
 
 function myFunction() {
-   document.getElementById('pocetna').scrollIntoView();
-  };
-  myFunction();
-
-  /* */
-
-  function myNewFunction(src){
-      window.location(src);
-  }
+  document.getElementById("pocetna").scrollIntoView();
+}
+myFunction();
